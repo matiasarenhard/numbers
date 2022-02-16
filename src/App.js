315de0ba82fba8152e6e19fea96287ee1feebb70
Matias <matias_arenhard@hotmail.com>
2022-02-15 23:07:04 -0300

    add class to control colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
-    <div>
-     hey
-    </div>
+    <>
+      <h1>{number}</h1>
+      <h2 className={number >= 0 ? "positive" : "negative"}>
+        {  number >= 0 ? "Positivo" : "Negativo"}
+      </h2>
+      <input type="text" name="number" value={number} onChange={(event) => setNumber(event.target.value)} />
+    </>
   );
 }
 
